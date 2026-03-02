151b32f60011888948ab6b6f7398187bdb98647c
yvineet2008 <yv4573@srmist.edu.in>
2026-03-02 22:43:51 +0530

    added uc1 Banner App

---
+++ src/oopsbannerappuc2.java
@@ -0,0 +1,13 @@
+public class oopsbannerappuc2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("   ***   " + "   ***   " + "  ******  " + "   ***** ");
+        System.out.println("  ** **  " + "  ** **  " + "  **   ** " + "  **   **");
+        System.out.println(" **   ** " + " **   ** " + "  **   ** " + "  **     ");
+        System.out.println(" **   ** " + " **   ** " + "  ******  " + "   ***** ");
+        System.out.println(" **   ** " + " **   ** " + "  **      " + "       **");
+        System.out.println("  ** **  " + "  ** **  " + "  **      " + "  **   **");
+        System.out.println("   ***   " + "   ***   " + "  **      " + "   ***** ");
+    }
+}
